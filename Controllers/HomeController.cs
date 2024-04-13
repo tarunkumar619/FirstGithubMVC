9c3cf2808c943e0dc0575cb28ff12d039e503c23
tarunkumar619 <100075259+tarunkumar619@users.noreply.github.com>
2024-04-13 11:32:53 +0530

    Update HomeController.cs homelona

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ public class HomeController : Controller
     {
         return View();
     }
-      public IActionResult Home()
+      public IActionResult Homeloan()
     {
         return View();
     }
