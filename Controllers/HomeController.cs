fac6357d1fcde96d912daf443be047988bd28e61
tarunkumar619 <100075259+tarunkumar619@users.noreply.github.com>
2024-04-13 11:36:03 +0530

    Update HomeController.cs add withdraw commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ public class HomeController : Controller
     {
         return View();
     }
-      public IActionResult Homeloan()
+      public IActionResult widthraw()
     {
         return View();
     }
